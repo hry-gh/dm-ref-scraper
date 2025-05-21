b94877d785eee209aa057d96a2fecf48ca36118d
harryob <55142896+harryob@users.noreply.github.com>
2025-05-21 07:16:48 +0100

    tidyup

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,16 @@ use toml_edit::{value, Array};
 struct Page {
     title: String,
     body: String,
-    extra: HashMap<String, Vec<String>>,
+}
+
+struct GlobalSelectors {
+    title_selector: Selector,
+    text_selector: Selector,
+    dl_selector: Selector,
+    b_selector: Selector,
+    dd_selector: Selector,
+
+    naive_stripper: Regex,
 }
 
 fn main() {
@@ -41,22 +50,26 @@ fn main() {
 
     let parts: Vec<&str> = raw.split("<hr>").collect();
 
-    let page_selector = &Selector::parse("a").unwrap();
-    let title_selector = &Selector::parse("h2").unwrap();
-    let text_selector = &Selector::parse("p,h3,xmp,pre,ul").unwrap();
-    let dl_selector = &Selector::parse("dl").unwrap();
+    let page_selector = Selector::parse("a").unwrap();
+    let title_selector = Selector::parse("h2").unwrap();
+    let text_selector = Selector::parse("p,h3,xmp,pre,ul").unwrap();
+    let dl_selector = Selector::parse("dl").unwrap();
 
-    let b_selector = &Selector::parse("b").unwrap();
-    let dd_selector = &Selector::parse("dd").unwrap();
+    let b_selector = Selector::parse("b").unwrap();
+    let dd_selector = Selector::parse("dd").unwrap();
 
     let naive_stripper = Regex::new("<a name.*></a>").unwrap();
+
+    let all_selectors = GlobalSelectors {
+        title_selector, text_selector, dl_selector, b_selector, dd_selector, naive_stripper
+    };
 
     let mut path_to_doc: HashMap<String, Html> = HashMap::new();
     let mut page_is_section: HashMap<String, bool> = HashMap::new();
     for page in parts.iter() {
         let document = Html::parse_document(page);
 
-        let Some(page_element) = document.select(page_selector).next() else {
+        let Some(page_element) = document.select(&page_selector).next() else {
             continue;
         };
 
@@ -75,75 +88,24 @@ fn main() {
 
     let mut path_to_page: HashMap<String, Page> = HashMap::new();
     for page_tuple in &path_to_doc {
-        let page_path = page_tuple.0;
-        let document = page_tuple.1;
-
-        let title = document.select(title_selector).next().unwrap().inner_html();
-
-        let mut extra_map: HashMap<String, Vec<String>> = HashMap::new();
-        for data_part in document.select(dl_selector) {
-            let Some(data_title_element) = data_part.select(b_selector).next() else {
-                continue;
-            };
-
-            let data_title = data_title_element
-                .inner_html()
-                .to_lowercase()
-                .replace(':', "")
-                .replace(' ', "_");
-
-            let mut opt_array: Vec<String> = Vec::new();
-            for results in data_part.select(dd_selector) {
-                let mut stripped = results.inner_html();
-
-                stripped = naive_stripper.replace_all(&stripped, "").to_string();
-
-                opt_array.push(parse_html_to_markdown(stripped, &page_is_section, &path_to_doc));
-            }
-
-            extra_map.insert(data_title, opt_array);
-        }
-
-        let mut text: Vec<String> = Vec::new();
-
-        for text_part in document.select(text_selector) {
-            match text_part.value().name() {
-                "p" => text.push(parse_html_to_markdown(text_part.inner_html(), &page_is_section, &path_to_doc)),
-                "h3" => text.push(format!("# {}", parse_html_to_markdown(text_part.inner_html(), &page_is_section, &path_to_doc))),
-                "xmp" => text.push(format!("```dm{}```", text_part.inner_html())),
-                "pre" => text.push(format!("```\n{}\n```", text_part.inner_html())),
-                "ul" => text.push(parse_html_to_markdown(text_part.html(), &page_is_section, &path_to_doc)),
-                _ => (),
-            }
-        }
-        path_to_page.insert(
-            page_path.to_string(),
-            Page {
-                title,
-                body: text.join("\n\n"),
-                extra: extra_map,
-            },
-        );
+        create_page_from_html(page_tuple.0, page_tuple.1, &mut path_to_page,  &path_to_doc, &all_selectors);
     }
 
-    path_to_page.insert("/".to_string(), Page { title: "Reference".to_string(), body: "".to_string(), extra: HashMap::new() });
+    path_to_page.insert("/".to_string(), Page { title: "Reference".to_string(), body: "".to_string() });
 
     for page in &path_to_page {
         let path = page.0;
         let page = page.1;
 
-        let mut section = false;
-
-        let mut path_str = path.clone();
+        let mut path_str = make_ref_web_safe(&path.clone());
 
         if let Some(is_section) = page_is_section.get(path) {
             if *is_section {
-                path_str = format!("{}/_index", path_str);
-                section = true;
+                path_str = format!("{}/index", path_str);
             }
         }
 
-        let clean_path = format!("{}{}", output_path, make_ref_web_safe(&path_str));
+        let clean_path = format!("{}{}", output_path, &path_str);
         let path = Path::new(&clean_path);
         let prefix = path.parent().unwrap();
         create_dir_all(prefix).unwrap();
@@ -153,41 +115,101 @@ fn main() {
         };
 
         let mut page_toml = toml_edit::DocumentMut::new();
-        page_toml["title"] = value(page.title.clone());
+        page_toml["title"] = value(page.title.replace("%%", r"\%\%"));
 
-
-        if section {
-            if let Some(segment) = Path::new(&path_str).parent() {
-                if let Some(segment) = segment.file_name() {
-                    if let Some(segment) = segment.to_str() {
-                        match segment {
-                            "proc" | "var" => {
-                                page_toml["page_template"] = value(format!("{}.html", segment))
-                            }
-                            _ => page_toml["template"] = value("object.html"),
-                        }
-                    }
-                }
-            }
+        if page.title == "DM language details" {
+            let mut al = Array::new();
+            al.push("index");
+            page_toml["alias"] = value(al);
         }
 
-        for title in page.extra.iter() {
-            let mut array = Array::new();
-
-            for val in title.1 {
-                array.push(val);
-            }
-
-            page_toml["extra"]["metadata"][title.0] = value(array);
-        }
-
-        let front_matter_and_body = format!("+++\n{}+++\n{}", page_toml, page.body.replace("&lt;", "<").replace("&gt;", ">"));
+        let front_matter_and_body = format!("+++\n{}+++\n{}", page_toml, remove_html_encode(&page.body));
 
         let _ = file.write_all(front_matter_and_body.as_bytes());
     }
 }
 
-fn parse_html_to_markdown(html: String, sections: &HashMap<String, bool>, all_pages: &HashMap<String, Html>) -> String {
+fn create_page_from_html(page_path: &String, document: &Html, path_to_page: &mut HashMap<String, Page>, path_to_doc: &HashMap<String, Html>, selectors: &GlobalSelectors) -> () {
+    let title = document.select(&selectors.title_selector).next().unwrap().inner_html();
+
+    let mut headers: Vec<(String, Vec<String>)> = Vec::new();
+    for data_part in document.select(&selectors.dl_selector) {
+        let Some(data_title_element) = data_part.select(&selectors.b_selector).next() else {
+            continue;
+        };
+
+        let data_title = data_title_element.inner_html().replace(':', "");
+
+        let mut opt_array: Vec<String> = Vec::new();
+        for results in data_part.select(&selectors.dd_selector) {
+            let mut stripped = results.inner_html();
+
+            stripped = selectors.naive_stripper.replace_all(&stripped, "").to_string();
+
+            opt_array.push(parse_html_to_markdown(stripped, &path_to_doc));
+        }
+
+        headers.push((data_title, opt_array));
+    }
+
+    let mut text: Vec<String> = Vec::new();
+
+    let mut write_after: Vec<String> = Vec::new();
+    for part in &headers {
+        let mut to_write= format!("### {}", part.0);
+
+
+        if part.1.len() > 1 {
+            to_write.push_str("\n");
+
+            for string in &part.1 {
+                to_write = format!("{}\n- {}", to_write, string.to_string());
+            }
+
+            to_write.push_str("\n");
+        } else if let Some(wrap) = part.1.first() {
+            to_write = format!("{}\n> {}", to_write, wrap)
+        }
+
+        if part.0 == "See also" {
+            write_after.push(to_write);
+        } else {
+            text.push(to_write);
+        }
+    }
+
+    for text_part in document.select(&selectors.text_selector) {
+        match text_part.value().name() {
+            "p" => {
+
+                if text_part.value().has_class("note", scraper::CaseSensitivity::CaseSensitive) {
+                    text.push(format!("> [!note]\n> {}", parse_html_to_markdown(text_part.inner_html(), path_to_doc)));
+                } else {
+                    text.push(parse_html_to_markdown(text_part.inner_html(), path_to_doc));
+                }
+            },
+            "h3" => text.push(format!("## {}", parse_html_to_markdown(text_part.inner_html(), path_to_doc))),
+            "xmp" => text.push(format!("```dream-maker\n{}\n```", text_part.inner_html().trim())),
+            "pre" => text.push(format!("```\n{}\n```", text_part.inner_html().trim())),
+            "ul" => text.push(parse_html_to_markdown(text_part.html(), path_to_doc)),
+            _ => (),
+        }
+    }
+
+    for part in write_after {
+        text.push(part);
+    }
+
+    path_to_page.insert(
+        page_path.to_string(),
+        Page {
+            title: remove_html_encode(&title),
+            body: text.join("\n\n"),
+        },
+    );
+}
+
+fn parse_html_to_markdown(html: String, all_pages: &HashMap<String, Html>) -> String {
     let code_regex = Regex::new("<(/)?(tt|code)>").unwrap();
     let a_link_selector: &Selector = &Selector::parse("a[href]").unwrap();
 
@@ -207,25 +229,15 @@ fn parse_html_to_markdown(html: String, sections: &HashMap<String, bool>, all_pa
                 }
             }
 
-            if let Some(_) = sections.get(&final_destination) {
-                html = html.replace(
-                    &link.html(),
-                    &format!(
-                        "[{}](@{}/_index.md)",
-                        link.inner_html(),
-                        make_ref_web_safe(&final_destination),
-                    ),
-                );
-            } else {
-                html = html.replace(
-                    &link.html(),
-                    &format!(
-                        "[{}](@{}.md)",
-                        link.inner_html(),
-                        make_ref_web_safe(&final_destination),
-                    ),
-                );
-            }            
+            html = html.replace(
+                &link.html(),
+                &format!(
+                    "[{}]({})",
+                    link.inner_html(),
+                    make_ref_web_safe(&final_destination),
+                ),
+            );
+
         }
     }
 
@@ -278,4 +290,8 @@ fn make_ref_web_safe(dirty_path: &String) -> String {
     path = clean_regex.replace_all(&path, "").to_string();
 
     path
+}
+
+fn remove_html_encode(dirty: &str) -> String {
+    dirty.replace("&amp;", "&").replace("&lt;", "<").replace("&gt;", ">")
 }
